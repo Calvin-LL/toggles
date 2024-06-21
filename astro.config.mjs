a41459aa7fbaad3f5424f3b717676fbe53108189
Calvin Liang <me@calvin.sh>
2024-06-21 16:02:54 -0700

    Revert "fix: fix build on cloudflare?"
    This reverts commit 60fe95df57fb56054145a8c9c963e23ba7430d29.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,4 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  vite: {
-    rollup: "npm:@rollup/wasm-node",
-  },
 });
