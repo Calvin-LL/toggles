52b3a4a194c843c99d0f0845fed1bc5fcd86d736
Calvin Liang <me@calvin.sh>
2024-06-21 15:53:23 -0700

    initial commit

---
+++ src/components/Toggles.tsx
@@ -0,0 +1,46 @@
+import { PlayProvider, withSharedState } from "@playhtml/react";
+// @ts-expect-error - no types
+import Switch from "react-ios-switch";
+
+export function TogglesWrapper() {
+  return (
+    <PlayProvider
+      initOptions={{
+        room: "toggles",
+      }}
+    >
+      <Toggles />
+    </PlayProvider>
+  );
+}
+
+interface TogglesProps {}
+
+export const Toggles = withSharedState(
+  { defaultData: { lastOn: -1 } },
+  ({ data, setData }, props: TogglesProps) => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "1rem",
+        }}
+      >
+        {Array.from({ length: 100 }).map((_, i) => (
+          <div key={i}>
+            <Switch
+              checked={i <= data.lastOn}
+              disabled={i > data.lastOn + 1}
+              onChange={(checked: boolean) => {
+                setData({ lastOn: checked ? i : i - 1 });
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
+);
